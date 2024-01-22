e9d2e85567ebf7b5227804cefce66224ca3c981b
arm00008 <155806568+arm00008@users.noreply.github.com>
2024-01-22 16:24:47 +0400

    BankAccount

---
+++ lesson_18/exercise_1.py
@@ -0,0 +1,13 @@
+# 1. Class Exercise:
+# Create a Python class representing a Hospital account with methods to schedule visit
+# and remove schedule
+
+class HospitalAccount:
+    def __init__(self):
+        self.shedule = {}
+
+    def shedule_visit(self, patinet_name, date_time):
+        if self.patient_name in self.shedule:
+            
+
+
